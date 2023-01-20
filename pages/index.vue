a1a946c0aefc08701139b4972b67ce92def6f748
澤田羽衣 <38057354+wc-sawada@users.noreply.github.com>
2023-01-20 18:22:39 +0900

    実験

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="list-group w-50 m-auto p-3">
+    <!--
+    <li v-for="item in results"
+        :key="item.id"
+        :id="item.id"
+        class="list-group-item d-flex align-items-center justify-content-between">
+      <p contenteditable class="mb-0">{{item.properties.Name.title[0].text.content}}</p>
+      <button @click="updateItem" class="btn btn-info flex-shrink-0">更新</button>
+    </li>
+    -->
+  </ul>
+</template>
+
+<script>
+import { Client } from '@notionhq/client';
+const notion = new Client({auth: process.env.NOTION_TOKEN});
+
+export default {
+  async asyncData() {
+    const myPage = await notion.databases.query({
+      database_id: process.env.DATABASE_ID,
+    })
+    console.log(myPage)
+    //return myPage
+  }
+}
+</script>
